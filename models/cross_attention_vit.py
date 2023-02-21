7cbc7eef767d391d50d042d1b7ab80e687fbda5d
hhroberthdaniel <hhroberthdaniel@gmail.com>
2023-02-21 16:27:24 +0200

    model v1

---
+++ models/cross_attention_vit.py
@@ -0,0 +1,326 @@
+import torch
+from einops import rearrange
+from einops import repeat
+from einops.layers.torch import Rearrange
+from torch import einsum
+from torch import nn
+
+# helpers
+
+def pair(t):
+    return t if isinstance(t, tuple) else (t, t)
+
+def exists(val):
+    return val is not None
+
+def default(val, d):
+    return val if exists(val) else d
+
+def stable_softmax(t, dim = -1):
+    t = t - t.amax(dim = dim, keepdim = True)
+    return t.softmax(dim = dim)
+# classes
+
+class PreNorm(nn.Module):
+    def __init__(self, dim, fn):
+        super().__init__()
+        self.norm = nn.LayerNorm(dim)
+        self.fn = fn
+    def forward(self, x, **kwargs):
+        return self.fn(self.norm(x), **kwargs)
+
+class FeedForward(nn.Module):
+    def __init__(self, dim, hidden_dim, dropout = 0.):
+        super().__init__()
+        self.net = nn.Sequential(
+            nn.Linear(dim, hidden_dim),
+            nn.GELU(),
+            nn.Dropout(dropout),
+            nn.Linear(hidden_dim, dim),
+            nn.Dropout(dropout)
+        )
+    def forward(self, x):
+        return self.net(x)
+
+class Attention(nn.Module):
+    def __init__(self, dim, heads = 8, dim_head = 64, dropout = 0.):
+        super().__init__()
+        inner_dim = dim_head *  heads
+        project_out = not (heads == 1 and dim_head == dim)
+
+        self.heads = heads
+        self.scale = dim_head ** -0.5
+
+        self.attend = nn.Softmax(dim = -1)
+        self.dropout = nn.Dropout(dropout)
+
+        self.to_qkv = nn.Linear(dim, inner_dim * 3, bias = False)
+
+        self.to_out = nn.Sequential(
+            nn.Linear(inner_dim, dim),
+            nn.Dropout(dropout)
+        ) if project_out else nn.Identity()
+
+    def forward(self, x):
+        qkv = self.to_qkv(x).chunk(3, dim = -1)
+        q, k, v = map(lambda t: rearrange(t, 'b n (h d) -> b h n d', h = self.heads), qkv)
+
+        dots = torch.matmul(q, k.transpose(-1, -2)) * self.scale
+
+        attn = self.attend(dots)
+        attn = self.dropout(attn)
+
+        out = torch.matmul(attn, v)
+        out = rearrange(out, 'b h n d -> b n (h d)')
+        return self.to_out(out)
+
+class Transformer(nn.Module):
+    def __init__(self, dim, depth, heads, dim_head, mlp_dim, dropout = 0.):
+        super().__init__()
+        self.layers = nn.ModuleList([])
+        for _ in range(depth):
+            self.layers.append(nn.ModuleList([
+                PreNorm(dim, Attention(dim, heads = heads, dim_head = dim_head, dropout = dropout)),
+                PreNorm(dim, FeedForward(dim, mlp_dim, dropout = dropout))
+            ]))
+    def forward(self, x):
+        for attn, ff in self.layers:
+            x = attn(x) + x
+            x = ff(x) + x
+        return x
+
+class ViT(nn.Module):
+    def __init__(self, *, image_size, patch_size, dim, depth, heads, mlp_dim, pool = 'cls', channels = 3, dim_head = 64, dropout = 0., emb_dropout = 0.):
+        super().__init__()
+        image_height, image_width = pair(image_size)
+        patch_height, patch_width = pair(patch_size)
+
+        assert image_height % patch_height == 0 and image_width % patch_width == 0, 'Image dimensions must be divisible by the patch size.'
+
+        num_patches = (image_height // patch_height) * (image_width // patch_width)
+        patch_dim = channels * patch_height * patch_width
+        assert pool in {'cls', 'mean'}, 'pool type must be either cls (cls token) or mean (mean pooling)'
+
+        self.to_patch_embedding = nn.Sequential(
+            Rearrange('b c (h p1) (w p2) -> b (h w) (p1 p2 c)', p1 = patch_height, p2 = patch_width),
+            nn.LayerNorm(patch_dim),
+            nn.Linear(patch_dim, dim),
+            nn.LayerNorm(dim),
+        )
+
+        self.pos_embedding = nn.Parameter(torch.randn(1, num_patches + 1, dim))
+        self.cls_token = nn.Parameter(torch.randn(1, 1, dim))
+        self.dropout = nn.Dropout(emb_dropout)
+
+        self.transformer = Transformer(dim, depth, heads, dim_head, mlp_dim, dropout)
+
+        self.pool = pool
+        self.to_latent = nn.Identity()
+
+        # self.mlp_head = nn.Sequential(
+        #     nn.LayerNorm(dim),
+        #     nn.Linear(dim, num_classes)
+        # )
+
+    def forward(self, img):
+        x = self.to_patch_embedding(img)
+        b, n, _ = x.shape
+
+        cls_tokens = repeat(self.cls_token, '1 1 d -> b 1 d', b = b)
+        x = torch.cat((cls_tokens, x), dim=1)
+        x += self.pos_embedding[:, :(n + 1)]
+        x = self.dropout(x)
+
+        x = self.transformer(x)
+
+        # x = x.mean(dim = 1) if self.pool == 'mean' else x[:, 0]
+
+        # x = self.to_latent(x)
+        # return self.mlp_head(x)
+        return x
+
+
+
+
+
+# bidirectional cross attention - have two sequences attend to each other with 1 attention step
+
+class BidirectionalCrossAttention(nn.Module):
+    def __init__(
+        self,
+        *,
+        dim,
+        heads = 8,
+        dim_head = 64,
+        context_dim = None,
+        context_out_dim=None,
+        dropout = 0.,
+        talking_heads = False,
+        prenorm = False,
+    ):
+        super().__init__()
+        context_dim = default(context_dim, dim)
+        context_out_dim = default(context_out_dim, context_dim)
+
+        self.norm = nn.LayerNorm(dim) if prenorm else nn.Identity()
+        self.context_norm = nn.LayerNorm(context_dim) if prenorm else nn.Identity()
+
+        self.heads = heads
+        self.scale = dim_head ** -0.5
+        inner_dim = dim_head * heads
+
+        self.dropout = nn.Dropout(dropout)
+        self.context_dropout = nn.Dropout(dropout)
+
+        self.to_qk = nn.Linear(dim, inner_dim, bias = False)
+        self.context_to_qk = nn.Linear(context_dim, inner_dim, bias = False)
+
+        self.to_v = nn.Linear(dim, inner_dim, bias = False)
+        self.context_to_v = nn.Linear(context_dim, inner_dim, bias = False)
+
+        self.to_out = nn.Linear(inner_dim, dim)
+        self.context_to_out = nn.Linear(inner_dim, context_out_dim)
+
+        self.talking_heads = nn.Conv2d(heads, heads, 1, bias = False) if talking_heads else nn.Identity()
+        self.context_talking_heads = nn.Conv2d(heads, heads, 1, bias = False) if talking_heads else nn.Identity()
+
+    def forward(
+        self,
+        x,
+        context,
+        mask = None,
+        context_mask = None,
+        return_attn = False,
+        rel_pos_bias = None
+    ):
+        b, i, j, h, device = x.shape[0], x.shape[-2], context.shape[-2], self.heads, x.device
+
+        x = self.norm(x)
+        context = self.context_norm(context)
+
+        # get shared query/keys and values for sequence and context
+
+        qk, v = self.to_qk(x), self.to_v(x)
+        context_qk, context_v = self.context_to_qk(context), self.context_to_v(context)
+
+        # split out head
+
+        qk, context_qk, v, context_v = map(lambda t: rearrange(t, 'b n (h d) -> b h n d', h = h), (qk, context_qk, v, context_v))
+
+        # get similarities
+
+        sim = einsum('b h i d, b h j d -> b h i j', qk, context_qk) * self.scale
+
+        # relative positional bias, if supplied
+
+        if exists(rel_pos_bias):
+            sim = sim + rel_pos_bias
+
+        # mask
+
+        if exists(mask) or exists(context_mask):
+            mask = default(mask, torch.ones((b, i), device = device, dtype = torch.bool))
+            context_mask = default(context_mask, torch.ones((b, j), device = device, dtype = torch.bool))
+
+            attn_mask = rearrange(mask, 'b i -> b 1 i 1') * rearrange(context_mask, 'b j -> b 1 1 j')
+            sim = sim.masked_fill(~attn_mask, -torch.finfo(sim.dtype).max)
+
+        # get attention along both sequence length and context length dimensions
+        # shared similarity matrix
+
+        attn = stable_softmax(sim, dim = -1)
+        context_attn = stable_softmax(sim, dim = -2)
+
+        # dropouts
+
+        attn = self.dropout(attn)
+        context_attn = self.context_dropout(context_attn)
+
+        # talking heads
+
+        attn = self.talking_heads(attn)
+        context_attn = self.context_talking_heads(context_attn)
+
+        # src sequence aggregates values from context, context aggregates values from src sequence
+
+        out = einsum('b h i j, b h j d -> b h i d', attn, context_v)
+        context_out = einsum('b h j i, b h j d -> b h i d', context_attn, v)
+
+        # merge heads and combine out
+
+        out, context_out = map(lambda t: rearrange(t, 'b h n d -> b n (h d)'), (out, context_out))
+
+        out = self.to_out(out)
+        context_out = self.context_to_out(context_out)
+
+        if return_attn:
+            return out, context_out, attn, context_attn
+
+        return out, context_out
+
+
+class CrossAttentionVit(nn.Module):
+    def __init__(self, image_size=1000, patch_size=100, vit_dim=1024, vit_depth=6, vit_heads=16, vit_mlp_dim=2048, vit_channels=1,
+                 ca_heads=8, ca_dim_head=64, idxs_dim=1, idxs_out_dim=1024, ca_num_layers=3,
+                 poly_trans_dim=1024, poly_trans_depth=3, poly_trans_head=8, poly_trans_dim_head=64, poly_trans_mlp_dim=1024
+                 , idxs_attention_num_layers=3):
+        super().__init__()
+        self.idxs_attention_num_layers = idxs_attention_num_layers
+        self.cross_atten_num_layers = ca_num_layers
+        self.vit = ViT(
+            image_size = image_size,
+            patch_size = patch_size,
+            dim = vit_dim,
+            depth = vit_depth,
+            heads = vit_heads,
+            mlp_dim = vit_mlp_dim,
+            channels=vit_channels,
+        )
+        self.ca_initial = BidirectionalCrossAttention(
+            dim = vit_dim,
+            heads = ca_heads,
+            dim_head = ca_dim_head,
+            context_dim = idxs_dim,
+            context_out_dim=idxs_out_dim,
+        )
+        self.ca_modules = nn.ModuleList([BidirectionalCrossAttention(
+            dim = vit_dim,
+            heads = ca_heads,
+            dim_head = ca_dim_head,
+        ) for _ in range(self.cross_atten_num_layers)])
+
+        self.poly_transformer = Transformer(dim=poly_trans_dim, depth=poly_trans_depth, heads=poly_trans_head, dim_head=poly_trans_dim_head, mlp_dim=poly_trans_mlp_dim)
+        self.poly_head = nn.Sequential(nn.Linear(in_features=1024, out_features=2), nn.Tanh())
+
+
+    def forward(self, x):
+        (states, poly_idxs, poly_mask) = x
+        image_embeddings = self.vit(states)
+        image_embeddings, poly_idxs = self.ca_initial(image_embeddings, poly_idxs, context_mask=poly_mask)
+        for ca in self.ca_modules:
+            image_embeddings, poly_idxs = ca(image_embeddings, poly_idxs, context_mask = poly_mask)
+        poly_features = self.poly_transformer(poly_idxs)
+        poly_features = self.poly_head(poly_features)
+        # poly_features[..., 0] *= self.max_grid_width
+        # poly_features[..., 1] *= self.max_grid_height
+        return poly_features
+
+
+if __name__ == "__main__":
+    m = CrossAttentionVit()
+    # states = torch.rand((1, 1, 1000, 1000))
+    states = torch.rand((1, 1, 1000, 1000))
+    poly_idxs = torch.rand((1, 100, 1))
+    poly_mask = torch.rand((1, 100)) < 0.5
+    x = (states, poly_idxs, poly_mask)
+    y = m(x)
+    print(y)
+
+
+
+
+
+
+
+
+
